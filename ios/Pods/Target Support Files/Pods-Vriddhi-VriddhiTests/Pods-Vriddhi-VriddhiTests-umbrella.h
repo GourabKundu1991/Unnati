5d1531fc265ecb84ee86fc444f47f2bfda9c6de6
“Gourab <gourab.pixzion@gmail.com>
2025-08-11 23:09:38 +0530

    New Branding

---
+++ ios/Pods/Target Support Files/Pods-Vriddhi-VriddhiTests/Pods-Vriddhi-VriddhiTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Unnati_UnnatiTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Unnati_UnnatiTestsVersionString[];
+
